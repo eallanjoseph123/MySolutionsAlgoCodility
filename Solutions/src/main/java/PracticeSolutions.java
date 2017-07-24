84c7bd46cf2e87b381dfd653c69f8fa5bf02961e
unknown <eallanjoseph@yahoo.com>
2017-07-24 16:40:18 +1200

    other solutions

--- Solutions/src/main/java/PracticeSolutions.java
+++ Solutions/src/main/java/PracticeSolutions.java
@@ -1,9 +1,50 @@
 package main.java;
 
+import java.util.Arrays;
+
 public class PracticeSolutions {
 
 	public static void main(String[] args) {
 		
+	}
+	
+	public static int oddOccurrencesInArray(int A[]){
+		Arrays.sort(A);
+		if(A.length == 1) return A[0];
+		for (int x = 1; x < A.length; x++) {
+			if( (x-1 )== 0 && A[x - 1] != A[x]){
+				return A[x - 1]; 
+			}
+			if(A[x - 1] == A[x] || ((x+1) < A.length && A[x] == A[x+1])){
+				continue;
+			}else{
+				return  A[x];
+			}
+		}
+		return 0;
+	}
+	
+	public static int[] cyclicRotation(int[] A, int K){
+		int []c = null;
+		if(A == null || A.length <= 1){
+			return A;
+		}
+		 for(int x = 0 ; x < K; x++){
+			 for(int y = 0 ; y < A.length; y++){
+				 if(y == 0){
+					 c = new int[A.length];
+					int f = A[y];
+					c[y] = A[A.length-1];
+					y = y + 1;
+					c[y] = f;
+				 }else{
+					 c[y] = A[y-1];
+				 }
+			 }A = c;
+			 c = null;
+		 }
+		 
+		return A;
 	}
 	
 	public static int binaryGap(int N){
@@ -13,9 +54,7 @@ public class PracticeSolutions {
 		int start = 0;
 		int start2 = 0;
 		int h = 0, l =0;
-		if(!binary.contains("0")){
-			return 0;
-		}
+		if(!binary.contains("0")){return 0;}
 		while(true){
 			index = binary.indexOf("1", start);
 			if (index != -1) {
--- Solutions/src/test/PracticeTest.java
+++ Solutions/src/test/PracticeTest.java
@@ -8,6 +8,44 @@ import main.java.PracticeSolutions;
 public class PracticeTest {
 	
 	@Test
+	public void oddOccurrencesInArrayTest(){
+		/*Assert.assertEquals(42, 
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{3,3,1,1,5,5,42,42}));*/
+		Assert.assertEquals(46, 
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{46}));
+		Assert.assertEquals(46,  
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{42,46,42}));
+		Assert.assertEquals(42, 
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{42}));
+		Assert.assertEquals(3, 
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{1,1,2,2,2,3,4,4}));
+		Assert.assertEquals(1, 
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{3,3,5,5,1,2,2,2,2,55,66,22,22,66,55}));
+		Assert.assertEquals(3, 
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{3,1,5,5,1}));
+		Assert.assertEquals(1, 
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{3,3,5,5,1}));
+		Assert.assertEquals(7, 
+				PracticeSolutions.oddOccurrencesInArray(new 
+						int[]{9,3,9,9,9,7,3}));
+	}
+	
+	@Test
+	public void cyclicRotationTest(){
+		Assert.assertArrayEquals(new int[]{9,7,6,3,8}, 
+				PracticeSolutions.cyclicRotation(new 
+						int[]{3,8,9,7,6},3));
+	}
+	
+	@Test
 	public void binaryGap(){
 		Assert.assertEquals(0, PracticeSolutions.binaryGap(15));
 		Assert.assertEquals(2, PracticeSolutions.binaryGap(9));
